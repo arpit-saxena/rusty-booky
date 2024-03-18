b9a533c5a5a11b45dff48bb8a0165bf7730b204a
Arpit Saxena <arpit.saxena2000@yahoo.in>
2024-03-18 17:29:53 +0530

    add new, prepend, tail methods

--- lists/src/third.rs
+++ lists/src/third.rs
@@ -10,3 +10,25 @@ struct Node<T> {
     elem: T,
     next: Link<T>,
 }
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        List { head: None }
+    }
+
+    pub fn prepend(&self, elem: T) -> List<T> {
+        let node = Node {
+            elem,
+            next: self.head.clone(),
+        };
+        List { head: Some(Rc::new(node)) }
+    }
+
+    pub fn tail(&self) -> List<T> {
+        List{head: self.head.as_ref().and_then(|node| node.next.clone())}
+    }
+
+    pub fn head(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| &node.elem)
+    }
+}
